4bb2d97db9fcef5c1aee86d586d473093da7ed30
Yücel <yucellgull@hotmail.com>
2022-12-03 15:46:26 +0300

    Add files via upload

---
+++ 7.Hafta Örnekler/Ornek1/Ornek1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrgDi13
+{   
+class Deneme 
+    {
+        public int a;
+        public int b;
+        public Deneme(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public static int Topla(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+       
+        PrgDi13.Deneme d = new PrgDi13.Deneme(1, 2);
+        Console.WriteLine(d.a);
+        Console.WriteLine(PrgDi13.Deneme.Topla(2, 3));
+    }
+}
---
+++ 7.Hafta Örnekler/Ornek2/Ornek2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace isimalan1
+{
+    class Deneme
+    {
+        public Deneme()
+        {
+            Console.WriteLine("Deneme 1");
+        }
+    }
+}
+namespace isimalan2
+{
+    class Deneme
+    {
+        public Deneme()
+        {
+            Console.WriteLine("Deneme 2");
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        isimalan1.Deneme d1 = new isimalan1.Deneme();
+        isimalan2.Deneme d2 = new isimalan2.Deneme();
+    }
+}
