0d2603c8ca32baba75d670fe2767da705a663369
Yücel <yucellgull@hotmail.com>
2022-12-03 15:04:35 +0300

    Add files via upload

---
+++ 1 ve 2. Hafta Örnekler/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class one
+{
+     static void Main()
+    {
+        object a = 120;
+        object b = "Bilgisayar";
+        int sayı =(int)a;
+        string str=(string)b;
+        Console.WriteLine("b string e cevrildi: "+str);
+        Console.WriteLine("a integer e çevrildi: " + sayı);
+    }
+}
---
+++ 1 ve 2. Hafta Örnekler/ConsoleApp2/ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class a
+{
+    static void Main()
+    {
+        object x;
+        x = 10;
+        Console.WriteLine(x.GetType());
+        x = "B";
+        Console.WriteLine(x.GetType());
+        x = 8.75F;
+        Console.WriteLine(x.GetType());
+        x = false;
+        Console.WriteLine(x.GetType());
+        x = 5.489M;
+        Console.WriteLine(x.GetType());
+    }
+}
+public class b
+{
+
+}
